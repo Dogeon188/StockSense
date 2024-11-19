f9cb67e7ddd8115027358bc66aa1ce098bd4ccdd
Dogeon188 <dogcatdog188@gmail.com>
2024-11-19 14:05:57 +0800

    client init

---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<main>
+    <h1>Hello world!</h1>
+    <p>
+        Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+    </p>
+</main>
+
+<style lang="sass">
+a
+    text-decoration: none
+</style>
