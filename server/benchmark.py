bc7ca2c160dd7711a2e44abffafa283d909ffaaa
Dogeon188 <dogcatdog188@gmail.com>
2024-12-02 21:39:58 +0800

    [Add] split data & basic training

--- server/benchmark.py
+++ server/benchmark.py
@@ -1,9 +1,11 @@
 import argparse
 import pandas as pd
 import asyncio
+from tqdm import tqdm
 
 from stockcore.parameters import Parameters, DataParameters
 from stocksense.api import data as scdata
+import stockcore.environment as scenv
 
 
 async def _load_data(data_params: DataParameters) -> pd.DataFrame:
@@ -20,7 +22,6 @@ async def _load_data(data_params: DataParameters) -> pd.DataFrame:
     return results
 
 def load_data(data_params: DataParameters) -> dict[str, pd.DataFrame]:
-    print("Loading data...")
     results = asyncio.run(_load_data(data_params))
     return dict(zip(data_params.symbols, results))
 
@@ -28,9 +29,44 @@ def load_data(data_params: DataParameters) -> dict[str, pd.DataFrame]:
 def main(config_path):
     # Load the configuration file
     params = Parameters.from_json(config_path)
-    data = load_data(params.data)
-    print(data)
 
+    # Load the data
+    print("Loading data...")
+    df_dict = load_data(params.data)
+    dfs = list(df_dict.values())
+
+    # Split the data
+
+    train_split_end = int(params.data.train_ratio * len(dfs[0]))
+    train_split_end_date = dfs[0].index[train_split_end]
+    val_split_end = int((params.data.train_ratio + params.data.val_ratio) * len(dfs[0]))
+    val_split_end_date = dfs[0].index[val_split_end]
+
+    train_split = {symbol: df[:train_split_end_date] for symbol, df in df_dict.items()}
+    val_split = {symbol: df[train_split_end_date:val_split_end_date] for symbol, df in df_dict.items()}
+    test_split = {symbol: df[val_split_end_date:] for symbol, df in df_dict.items()}
+
+    # Create the environment
+    print("Creating environment...")
+    env = scenv.MultiStockTradingEnv(
+        dfs=list(train_split.values())[:2],
+        name="StockTradingEnv@{}".format(config_path),
+        portfolio_initial_value=params.environment.initial_amount,
+        trading_fees=params.environment.trading_fee,
+        verbose=0
+    )
+
+    done, truncated = False, False
+    observation = env.reset()
+    pbar = tqdm(total=env.get_dfs_length())
+    while not done and not truncated:
+        # Pick a position by its index in your position list (=[-1, 0, 1])....usually something like : position_index = your_policy(observation)
+        # random policy
+        position_index = env.action_space.sample() # At every timestep, pick a random position index from your position list (=[-1, 0, 1])
+        pbar.update(1)
+        observation, reward, done, truncated, info = env.step(position_index)
+    pbar.close()
+    env.log()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
--- server/stockcore/environment/customenv.py
+++ server/stockcore/environment/customenv.py
@@ -4,36 +4,47 @@ import pandas as _pd
 import numpy as _np
 import stockcore.data as _scdata
 
+
 class History:
     def __init__(self):
         self.total_assets_list = []
+
     def add(self, total_assets):
         self.total_assets_list.append(total_assets)
+
     def get_total_assets(self, idx):
         return self.total_assets_list[idx]
+
 
 class Portfolio:
     def __init__(self, percentages_of_stocks_and_cash, total_money, prices_of_stocks):
         self.amount_of_stocks = []
         for idx, percentage in enumerate(percentages_of_stocks_and_cash[:-1]):
-            self.amount_of_stocks.append(percentage * total_money / prices_of_stocks[idx])
+            self.amount_of_stocks.append(
+                percentage * total_money / prices_of_stocks[idx])
         self.cash = percentages_of_stocks_and_cash[-1] * total_money
+
     def total_assets(self, prices_of_stocks):
         return sum([amount * price for amount, price in zip(self.amount_of_stocks, prices_of_stocks)]) + self.cash
-    def trade_to_new_percentages(self, percentages_of_stocks_and_cash, prices_of_stocks, trading_fees):   
+
+    def trade_to_new_percentages(self, percentages_of_stocks_and_cash, prices_of_stocks, trading_fees):
         total_money = self.total_assets(prices_of_stocks)
         for idx, (percentage, price) in enumerate(zip(percentages_of_stocks_and_cash[:-1], prices_of_stocks)):
-            stock_trade = (percentage * total_money / price) - self.amount_of_stocks[idx]
+            stock_trade = (percentage * total_money / price) - \
+                self.amount_of_stocks[idx]
             if stock_trade > 0:
-                stock_trade = stock_trade / (1 - trading_fees + trading_fees * percentage)
+                stock_trade = stock_trade / \
+                    (1 - trading_fees + trading_fees * percentage)
                 cash_trade = - stock_trade * price
-                self.amount_of_stocks[idx] = self.amount_of_stocks[idx] + stock_trade * (1 - trading_fees)
+                self.amount_of_stocks[idx] = self.amount_of_stocks[idx] + \
+                    stock_trade * (1 - trading_fees)
                 self.cash = self.cash + cash_trade
             else:
                 stock_trade = stock_trade / (1 - trading_fees * percentage)
                 cash_trade = - stock_trade * price
                 self.amount_of_stocks[idx] = self.amount_of_stocks[idx] + stock_trade
                 self.cash = self.cash + cash_trade * (1 - trading_fees)
+
 
 class MultiStockTradingEnv(_gym.Env):
     """This class is a custom environment for multi stock trading. This is different from the `MultiDatasetTradingEnv` environment in gym-trading-env, as it allows to trade multiple stocks at the same time.   
@@ -42,15 +53,15 @@ class MultiStockTradingEnv(_gym.Env):
     metadata = {'render_modes': ['logs']}
 
     def __init__(self,
-                dfs : list,
-                windows = None,
-                trading_fees = 0,
-                portfolio_initial_value = 1000,
-                max_episode_duration = 'max',
-                verbose = 1,
-                name = "Stock",
-                render_mode= "logs"
-                ):
+                 dfs: list[_pd.DataFrame],
+                 windows=None,
+                 trading_fees=0,
+                 portfolio_initial_value=1000,
+                 max_episode_duration='max',
+                 verbose=1,
+                 name="Stock",
+                 render_mode="logs"
+                 ):
         self.max_episode_duration = max_episode_duration
         self.verbose = verbose
         self.name = name
@@ -59,80 +70,84 @@ class MultiStockTradingEnv(_gym.Env):
         self.windows = windows
         self.trading_fees = trading_fees
         self.portfolio_initial_value = float(portfolio_initial_value)
-        
+
         self._set_dfs(dfs)
-        
+
         self.number_of_stocks = len(dfs)
         self.action_space = spaces.Discrete(self.number_of_stocks + 1)
         self.observation_space = spaces.Box(
             -_np.inf,
             _np.inf,
-            shape = [self._nb_features]
+            shape=[self._nb_features]
         )
         if self.windows is not None:
             self.observation_space = spaces.Box(
                 -_np.inf,
                 _np.inf,
-                shape = [self.windows, self._nb_features]
+                shape=[self.windows, self._nb_features]
             )
-        
+
         self.log_metrics = []
 
-
-    def _set_dfs(self, dfs):
+    def _set_dfs(self, dfs: list[_pd.DataFrame]):
         dfs = [df.copy() for df in dfs]
         self.dfs = dfs
         self._number_of_stocks = len(dfs)
         merged_df = self._dfs_preprocess(dfs)
         self.length_of_merged_df = len(merged_df)
 
-        self._features_columns = [col for col in merged_df.columns if "feature" in col]
+        self._features_columns = [
+            col for col in merged_df.columns if "feature" in col]
         self._nb_features = len(self._features_columns)
 
-        self._obs_array = _np.array(merged_df[self._features_columns], dtype= _np.float32)
+        self._obs_array = _np.array(
+            merged_df[self._features_columns], dtype=_np.float32)
         self._price_array = [df["close"] for df in self.dfs]
 
-    def _dfs_preprocess(self, dfs):
+    def _dfs_preprocess(self, dfs: list[_pd.DataFrame]):
         dfs = [df.copy() for df in dfs]
         dfs = [_scdata.data_preprocess(df, dropna=False) for df in dfs]
         for idx, df in enumerate(dfs):
             df.columns += f"_{idx}"
-        merged_df = _pd.concat(dfs, axis = 1)
+        merged_df = _pd.concat(dfs, axis=1)
         merged_df.dropna(inplace=True)
         return merged_df
 
     def get_dfs_length(self):
         return self.length_of_merged_df
-    
-    def _get_price(self, delta = 0):
-        return [price[self._idx + delta] for price in self._price_array]
-    
+
+    def _get_price(self, delta=0):
+        return [price.iloc[self._idx + delta] for price in self._price_array]
+
     def _get_obs(self):
 
         if self.windows is None:
             _step_index = self._idx
-        else: 
-            _step_index = _np.arange(self._idx + 1 - self.windows , self._idx + 1)
+        else:
+            _step_index = _np.arange(
+                self._idx + 1 - self.windows, self._idx + 1)
         return self._obs_array[_step_index]
-    
-    def reset(self, seed = None, options=None):
-        super().reset(seed = seed)
-        
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+
         self._step = 0
-        self._percentages_of_stocks_and_cash = [0] * self.number_of_stocks + [1] # 1 for cash
-        
+        self._percentages_of_stocks_and_cash = [
+            0] * self.number_of_stocks + [1]  # 1 for cash
+
         self._idx = 0
-        if self.windows is not None: self._idx = self.windows - 1
+        if self.windows is not None:
+            self._idx = self.windows - 1
         if self.max_episode_duration != 'max':
             self._idx = _np.random.randint(
-                low = self._idx, 
-                high = len(self.df) - self.max_episode_duration - self._idx
+                low=self._idx,
+                high=len(self.df) - self.max_episode_duration - self._idx
             )
-        
-        self._portfolio  = Portfolio(
-            percentages_of_stocks_and_cash = self._percentages_of_stocks_and_cash,
-            total_money = self.portfolio_initial_value,
-            prices_of_stocks = self._get_price()
+
+        self._portfolio = Portfolio(
+            percentages_of_stocks_and_cash=self._percentages_of_stocks_and_cash,
+            total_money=self.portfolio_initial_value,
+            prices_of_stocks=self._get_price()
         )
 
         self.historical_info = History()
@@ -140,11 +155,11 @@ class MultiStockTradingEnv(_gym.Env):
 
         return self._get_obs()
 
-    def _trade(self, percentages_of_stocks_and_cash, prices_of_stocks = None):
+    def _trade(self, percentages_of_stocks_and_cash, prices_of_stocks=None):
         self._portfolio.trade_to_new_percentages(
-            percentages_of_stocks_and_cash = percentages_of_stocks_and_cash,
-            prices_of_stocks = self._get_price() if prices_of_stocks is None else prices_of_stocks,
-            trading_fees = self.trading_fees
+            percentages_of_stocks_and_cash=percentages_of_stocks_and_cash,
+            prices_of_stocks=self._get_price() if prices_of_stocks is None else prices_of_stocks,
+            trading_fees=self.trading_fees
         )
         self._percentages_of_stocks_and_cash = percentages_of_stocks_and_cash
         return
@@ -160,9 +175,10 @@ class MultiStockTradingEnv(_gym.Env):
 
     def reward_function(self):
         return _np.log(self.historical_info.get_total_assets(-1) / self.historical_info.get_total_assets(-2))
-    
-    def step(self, actions = None):
-        if actions is not None: self._take_action(actions)
+
+    def step(self, actions=None):
+        if actions is not None:
+            self._take_action(actions)
         self._idx += 1
         self._step += 1
 
@@ -175,7 +191,7 @@ class MultiStockTradingEnv(_gym.Env):
             done = True
         if self._idx >= self.length_of_merged_df - 1:
             truncated = True
-        if isinstance(self.max_episode_duration,int) and self._step >= self.max_episode_duration - 1:
+        if isinstance(self.max_episode_duration, int) and self._step >= self.max_episode_duration - 1:
             truncated = True
 
         self.historical_info.add(portfolio_value)
@@ -185,12 +201,13 @@ class MultiStockTradingEnv(_gym.Env):
             info = None
 
         if done or truncated:
-            self.log()
-        return self._get_obs(),  reward, done, truncated, info
+            if self.verbose > 0:
+                self.log()
+        return self._get_obs(), reward, done, truncated, info
 
     def log(self):
-        if self.verbose > 0:
-            print("Portfolio Return : " + f"{100*(self.historical_info.get_total_assets(-1) / self.historical_info.get_total_assets(0) -1):5.2f}%")
+        print("Portfolio Return : " + f"{100*(self.historical_info.get_total_assets(
+            -1) / self.historical_info.get_total_assets(0) - 1):5.2f}%")
 
     def render():
         pass
